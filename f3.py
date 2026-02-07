eb7cae5ad4da813cbd157e8908f0acf6b963ae6b
pushkalmehrotra <pushkalmehrotra774@gmail.com>
2026-02-07 16:20:14 +0530

    initial.commit

---
+++ f3.py
@@ -0,0 +1,8 @@
+file = open("codingal.txt","r")
+counter = 0
+content = file.read()
+CoList = Content.split("\n")
+print (Colist)
+Counter=len (Colist)
+print("This is the number of lines in the file")
+print (Counter)
